0fb6c69df827a3a65f9ac02d113e0286e2fd495b
drakkar1969 <nicolabay@outlook.com>
2023-08-29 10:42:48 +0200

    Install file roller in GNOME extras

--- arch-post-install.bash
+++ arch-post-install.bash
@@ -382,7 +382,7 @@ install_gnome()
 		pacman -S --asdeps system-config-printer
 
 		print_progress_text "Installing GNOME Extras"
-		pacman -S gnome-tweaks dconf-editor simple-scan
+		pacman -S gnome-tweaks file-roller dconf-editor simple-scan
 
 		print_progress_text "Enabling Wayland Screen Sharing"
 		pacman -S --asdeps --needed xdg-desktop-portal-gnome
